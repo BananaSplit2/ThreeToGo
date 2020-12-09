ec2b00770c128967bfe4fee9cc224d84de6b6e4b
BananaSplit2 <derkeethus96@gmail.com>
2020-12-09 14:21:54 +0100

    Correction structure token

--- src/threetogo.h
+++ src/threetogo.h
@@ -1,6 +1,9 @@
-typedef struc token{
+typedef struct token{
     int couleur;
     int forme;
     struct token* suivant;
-    struct token* precedent;
+    struct token* suivant_couleur;
+    struct token* precedent_couleur;
+    struct token* suivant_forme;
+    struct token* precedent_forme;
 }Tokens, *Liste;
